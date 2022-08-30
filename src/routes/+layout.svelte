bbf282974d0c49103511792a849d53daef0364e1
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-08-30 00:17:25 -0400

    added umami tracking code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { navigating } from '$app/stores';
-	import Footer from '$lib/layout/footer/Footer.svelte';
 	import '../app.postcss';
 
 	import Loader from '$lib/loading/Loader.svelte';
@@ -8,6 +7,14 @@
 
 	$: loading.setNavigate(!!$navigating);
 </script>
+
+<svelte:head>
+	<script
+		async
+		defer
+		data-website-id="a504dca9-1d40-4994-9ab4-23eb03a81fa0"
+		src="https://analytics.aemers.com/umami.js"></script>
+</svelte:head>
 
 <Loader />
 <slot />
