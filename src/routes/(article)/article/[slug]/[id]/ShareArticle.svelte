3d412f572910c66e76c4b5107aeae489370c8372
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-10-11 17:12:47 -0400

    experimenting fb share

--- src/routes/(article)/article/[slug]/[id]/ShareArticle.svelte
+++ src/routes/(article)/article/[slug]/[id]/ShareArticle.svelte
@@ -1,12 +1,29 @@
 <script>
 	import { EmailSolid, FacebookSolid, LinkedInSolid, LinkSolid, Share } from '$lib/svg';
+	import { onMount } from 'svelte';
+
+	let url;
+
+	onMount(() => {
+		url = window.location.href;
+	});
+
+	export let title = '';
+
+	$: if (title.includes(' ')) title = title.replaceAll(' ', '%20');
 </script>
 
 <div class="flex items-center gap-x-2">
 	<span class="w-3 h-3 lg:w-4 lg:h-4 text-primary"><Share /></span>
 	<div class="flex items-center gap-x-2 lg:gap-x-3 text-sm lg:text-base text-secondary">
 		Share to:
-		<span class="w-4 h-4 lg:w-5 lg:h-5"><FacebookSolid /></span>
+		<a
+			href={'https://facebook.com/sharer/sharer.php?u=' + url}
+			target="_blank"
+			rel="noopener"
+			aria-label="Share on Facebook"
+			class="w-4 h-4 lg:w-5 lg:h-5"><FacebookSolid /></a
+		>
 		<span class="w-4 h-4 lg:w-5 lg:h-5"><LinkedInSolid /></span>
 		<span class="w-4 h-4 lg:w-5 lg:h-5"><EmailSolid /></span>
 		<span class="w-4 h-4 lg:w-5 lg:h-5"><LinkSolid /></span>
